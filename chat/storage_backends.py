7890a285d1d7dadb0dfe31f5141f6e2fb189b66d
John Thuku <ceogachone@gmail.com>
2025-10-18 09:34:55 +0300

    media file upload path fixed

--- chat/storage_backends.py
+++ chat/storage_backends.py
@@ -3,6 +3,10 @@ from storages.backends.sftpstorage import SFTPStorage
 
 
 class CPanelSFTPStorage(SFTPStorage):
+    """
+    Custom SFTP Storage for cPanel hosting.
+    Uploads media files to remote server via SFTP.
+    """
     
     def __init__(self, **kwargs):
         kwargs['host'] = settings.SFTP_STORAGE_HOST
@@ -13,8 +17,12 @@ class CPanelSFTPStorage(SFTPStorage):
         kwargs['dir_mode'] = settings.SFTP_STORAGE_DIR_MODE
         super().__init__(**kwargs)
     
+    def _save(self, name, content):
+        name = name.replace('\\', '/')
+        return super()._save(name, content)
+    
     def url(self, name):
         if name:
-            clean_name = name.lstrip('/')
+            clean_name = name.lstrip('/').replace('\\', '/')
             return f"{settings.MEDIA_URL.rstrip('/')}/{clean_name}"
         return settings.MEDIA_URL
